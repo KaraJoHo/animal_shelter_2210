5ce0329c521f9d0b0492f68d2499b433fd88a9c0
KaraJH <karas.jones@yahoo.com>
2022-10-12 09:59:40 -0600

    Completed tests and call_pets, patrons, methods

--- lib/shelter.rb
+++ lib/shelter.rb
@@ -1,14 +1,23 @@
 class Shelter
-  attr_reader :name, :capacity, :pets
+  attr_reader :name, :capacity, :pets, :excla_pets
 
   def initialize(shelter_name, shelter_capacity)
     @name = shelter_name
     @capacity = shelter_capacity
     @pets = []
+    @excla_pets = []
   end
 
   def add_pet(pet_name)
     pets << pet_name
+  end
+
+  def patrons
+    @pets
+  end
+
+  def call_pets
+    pets.map {|pet| "#{pet}!"}
   end
 
 end

b741b8305778b5af9fe0387408c9ac4dba39e86f
KaraJH <karas.jones@yahoo.com>
2022-10-12 09:36:51 -0600

    Completed iteration1 tests and instance variables defined

--- lib/shelter.rb
+++ lib/shelter.rb
@@ -1,2 +1,14 @@
 class Shelter
+  attr_reader :name, :capacity, :pets
+
+  def initialize(shelter_name, shelter_capacity)
+    @name = shelter_name
+    @capacity = shelter_capacity
+    @pets = []
+  end
+
+  def add_pet(pet_name)
+    pets << pet_name
+  end
+
 end
